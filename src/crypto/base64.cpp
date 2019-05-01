eb9ca0c3158a185a45b78ea6e98b2c08145f69d1
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2019-05-01 12:26:45 -0400

    base64 cleanup

--- src/crypto/base64.cpp
+++ src/crypto/base64.cpp
@@ -29,33 +29,22 @@
 
 namespace fc {
 
-inline const std::string& base64_chars()
-{
-    static const std::string m_base64_chars = 
+static constexpr char base64_chars[] =
                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                  "abcdefghijklmnopqrstuvwxyz"
                  "0123456789+/";
-    return m_base64_chars;
-}
-
-inline const std::string& base64url_chars()
-{
-    static const std::string m_base64_chars = 
+static constexpr char base64url_chars[] =
                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                  "abcdefghijklmnopqrstuvwxyz"
                  "0123456789-_";
-    return m_base64_chars;
+
+static_assert(sizeof(base64_chars) == sizeof(base64url_chars), "base64 and base64url must have the same amount of chars");
+
+static inline bool is_base64(unsigned char c, const char* const b64_chars) {
+  return (isalnum(c) || (c == b64_chars[sizeof(base64_chars)-3]) || (c == b64_chars[sizeof(base64_chars)-2]));
 }
 
-static inline bool is_base64(unsigned char c) {
-  return (isalnum(c) || (c == '+') || (c == '/'));
-}
-
-static inline bool is_base64url(unsigned char c) {
-  return (isalnum(c) || (c == '-') || (c == '_'));
-}
-
-std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+std::string base64_encode_impl(unsigned char const* bytes_to_encode, unsigned int in_len, const char* const b64_chars) {
 
   std::string ret;
   int i = 0;
@@ -72,7 +61,7 @@ std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_
       char_array_4[3] = char_array_3[2] & 0x3f;
 
       for(i = 0; (i <4) ; i++)
-        ret += base64_chars()[char_array_4[i]];
+        ret += b64_chars[char_array_4[i]];
       i = 0;
     }
   }
@@ -88,7 +77,7 @@ std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_
     char_array_4[3] = char_array_3[2] & 0x3f;
 
     for (j = 0; (j < i + 1); j++)
-      ret += base64_chars()[char_array_4[j]];
+      ret += b64_chars[char_array_4[j]];
 
     while((i++ < 3))
       ret += '=';
@@ -99,6 +88,9 @@ std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_
 
 }
 
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+   return base64_encode_impl(bytes_to_encode, in_len, base64_chars);
+}
 
 std::string base64_encode( const std::string& enc ) {
   char const* s = enc.c_str();
@@ -106,47 +98,7 @@ std::string base64_encode( const std::string& enc ) {
 }
 
 std::string base64url_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
-
-  std::string ret;
-  int i = 0;
-  int j = 0;
-  unsigned char char_array_3[3];
-  unsigned char char_array_4[4];
-
-  while (in_len--) {
-    char_array_3[i++] = *(bytes_to_encode++);
-    if (i == 3) {
-      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-      char_array_4[3] = char_array_3[2] & 0x3f;
-
-      for(i = 0; (i <4) ; i++)
-        ret += base64url_chars()[char_array_4[i]];
-      i = 0;
-    }
-  }
-
-  if (i)
-  {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
-
-    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-    char_array_4[3] = char_array_3[2] & 0x3f;
-
-    for (j = 0; (j < i + 1); j++)
-      ret += base64url_chars()[char_array_4[j]];
-
-    while((i++ < 3))
-      ret += '=';
-
-  }
-
-  return ret;
-
+   return base64_encode_impl(bytes_to_encode, in_len, base64url_chars);
 }
 
 std::string base64url_encode( const std::string& enc ) {
@@ -154,7 +106,7 @@ std::string base64url_encode( const std::string& enc ) {
   return base64url_encode( (unsigned char const*)s, enc.size() );
 }
 
-std::string base64_decode(std::string const& encoded_string) {
+std::string base64_decode_impl(std::string const& encoded_string, const char* const b64_chars) {
   int in_len = encoded_string.size();
   int i = 0;
   int j = 0;
@@ -162,11 +114,11 @@ std::string base64_decode(std::string const& encoded_string) {
   unsigned char char_array_4[4], char_array_3[3];
   std::string ret;
 
-  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_], b64_chars)) {
     char_array_4[i++] = encoded_string[in_]; in_++;
     if (i ==4) {
       for (i = 0; i <4; i++)
-        char_array_4[i] = base64_chars().find(char_array_4[i]);
+        char_array_4[i] = strchr(b64_chars, char_array_4[i]) - b64_chars;
 
       char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
       char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
@@ -183,7 +135,7 @@ std::string base64_decode(std::string const& encoded_string) {
       char_array_4[j] = 0;
 
     for (j = 0; j <4; j++)
-      char_array_4[j] = base64_chars().find(char_array_4[j]);
+      char_array_4[j] = strchr(b64_chars, char_array_4[j]) - b64_chars;
 
     char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
     char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
@@ -195,45 +147,12 @@ std::string base64_decode(std::string const& encoded_string) {
   return ret;
 }
 
+std::string base64_decode(std::string const& encoded_string) {
+   return base64_decode_impl(encoded_string, base64_chars);
+}
+
 std::string base64url_decode(std::string const& encoded_string) {
-  int in_len = encoded_string.size();
-  int i = 0;
-  int j = 0;
-  int in_ = 0;
-  unsigned char char_array_4[4], char_array_3[3];
-  std::string ret;
-
-  while (in_len-- && ( encoded_string[in_] != '=') && is_base64url(encoded_string[in_])) {
-    char_array_4[i++] = encoded_string[in_]; in_++;
-    if (i ==4) {
-      for (i = 0; i <4; i++)
-        char_array_4[i] = base64url_chars().find(char_array_4[i]);
-
-      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-      for (i = 0; (i < 3); i++)
-        ret += char_array_3[i];
-      i = 0;
-    }
-  }
-
-  if (i) {
-    for (j = i; j <4; j++)
-      char_array_4[j] = 0;
-
-    for (j = 0; j <4; j++)
-      char_array_4[j] = base64url_chars().find(char_array_4[j]);
-
-    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
-  }
-
-  return ret;
+   return base64_decode_impl(encoded_string, base64url_chars);
 }
 
 } // namespace fc
